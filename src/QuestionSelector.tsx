deba4869e9d7a74ab74624bf65253e6fdd9ac2e8
Philippe Voinov <philippevoinov@gmail.com>
2021-09-25 13:58:23 +0200

    New search UI

--- src/QuestionSelector.tsx
+++ src/QuestionSelector.tsx
@@ -1,146 +1,152 @@
-import { Question } from "./Interfaces";
-import Autosuggest from "react-autosuggest";
+import { css } from "@emotion/css";
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from "constants";
 import Fuse from "fuse.js";
-
-import { useState } from "react";
+import { useEffect } from "react";
+import { useMemo, useState } from "react";
+import { Question } from "./Interfaces";
 
 const fuseOptions = {
-	// isCaseSensitive: false,
-	// includeScore: false,
-	// shouldSort: true,
-	// includeMatches: false,
-	// findAllMatches: false,
-	// minMatchCharLength: 1,
-	// location: 0,
-	threshold: 0.6,
-	// distance: 100,
-	// useExtendedSearch: false,
-	// ignoreLocation: false,
-	// ignoreFieldNorm: false,
-	keys: ["text"],
+  // isCaseSensitive: false,
+  // includeScore: false,
+  // shouldSort: true,
+  // includeMatches: false,
+  // findAllMatches: false,
+  // minMatchCharLength: 1,
+  // location: 0,
+  threshold: 0.6,
+  // distance: 100,
+  // useExtendedSearch: false,
+  // ignoreLocation: false,
+  // ignoreFieldNorm: false,
+  keys: ["text"],
 };
 
-const theme = {
-	container: { width: "500px" },
-	input: {
-		width: "458px",
-		height: 30,
-		padding: "10px 20px",
-		fontFamily: "Helvetica, sans-serif",
-		fontWeight: 300,
-		fontSize: 16,
-		border: "1px solid black",
-		borderTopLeftRadius: 4,
-		borderTopRightRadius: 4,
-		borderBottomLeftRadius: 4,
-		borderBottomRightRadius: 4,
-	},
-	suggestionsList: {
-		margin: 0,
-		padding: 0,
-		listStyleType: "none",
-	},
-	suggestion: {
-		cursor: "pointer",
-		padding: "10px 20px",
-		marginTop: 10,
-		border: "1px solid black",
-		borderRadius: 10,
-	},
-	suggestionHighlighted: {
-		backgroundColor: "orange",
-	},
+const styles = {
+  wrapper: css`
+    max-width: 800px;
+    margin: 0 auto;
+    margin-top: 300px;
+  `,
+  questionInput: css`
+    border: none;
+    padding: 0 10px;
+    margin: 0;
+    margin-bottom: 30px;
+    width: 240px;
+    background: none;
+    font-size: 45px;
+    font-weight: 500;
+    width: 100%;
+    box-sizing: border-box;
+  `,
+  suggestion: css`
+    position: relative;
+    font-size: 45px;
+    width: 100%;
+    padding: 5px 20px;
+    box-sizing: border-box;
+  `,
+  activeSuggestion: css`
+    background: #743fca;
+    color: white;
+  `,
+  enterHint: css`
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: calc(100% + 10px);
+    height: 100%;
+    background: #743fca;
+    font-size: 20px;
+    text-align: center;
+    padding: 0 10px;
+  `,
 };
+
+const maxSuggestions = 3;
 
 interface Props {
-	questions: Question[];
-	onSelect: (id: number | undefined) => void;
+  questions: Question[];
+  onSelect: (id: number | undefined) => void;
 }
+
 const QuestionSelector: React.FC<Props> = ({ questions, onSelect }) => {
-	const [searchValue, setSearchValue] = useState("");
-	const [suggestions, setSuggestions] = useState<Question[]>([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-	const fuse = new Fuse(questions, fuseOptions);
+  const fuse = useMemo(() => new Fuse(questions, fuseOptions), [questions]);
+  const suggestions = useMemo(() => {
+    const inputValue = searchValue.trim().toLowerCase();
+    const inputLength = inputValue.length;
 
-	const onChange = (event: any, { newValue }: { newValue: string }) => {
-		setSearchValue(newValue);
-	};
+    return inputLength === 0
+      ? []
+      : fuse.search(searchValue).slice(0, maxSuggestions);
+  }, [searchValue, fuse]);
 
-	const getSuggestions = (value: string) => {
-		const inputValue = value.trim().toLowerCase();
-		const inputLength = inputValue.length;
+  useEffect(() => {
+    setSelectedIndex(0);
+  }, [suggestions]);
 
-		return inputLength === 0
-			? questions
-			: fuse.search(value).map((v) => v.item);
-	};
+  useEffect(() => {
+    const onKeyDown = (ev: KeyboardEvent) => {
+      if (ev.key === "ArrowUp" || ev.key === "ArrowDown") {
+        ev.preventDefault();
+        ev.stopPropagation();
+        const offset = ev.key === "ArrowUp" ? -1 : 1;
+        setSelectedIndex((i) =>
+          Math.max(
+            0,
+            Math.min(suggestions.length - 1, maxSuggestions - 1, i + offset)
+          )
+        );
+      }
+    };
 
-	const getSuggestionValue = (question: Question) => question.text;
+    document.addEventListener("keydown", onKeyDown);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  });
 
-	const renderQuestion = (question: Question) => <div>{question.text}</div>;
-
-	const onSuggestionsFetchRequested = ({ value }: { value: string }) => {
-		setSuggestions(getSuggestions(value));
-	};
-
-	// Autosuggest will call this function every time you need to clear suggestions.
-	const onSuggestionsClearRequested = () => {
-		setSuggestions([]);
-	};
-
-	const onSuggestionSelected = (
-		event: any,
-		{ suggestion }: { suggestion: any }
-	) => {
-		onSelect(suggestion.id);
-	};
-
-	const inputProps = {
-		placeholder: "Ask your question...",
-		value: searchValue,
-		onChange: onChange,
-	};
-	return (
-		<div
-			style={{
-				width: "100%",
-				height: "100%",
-				display: "flex",
-				justifyContent: "center",
-				marginTop: 300,
-			}}
-		>
-			<div style={{ width: "500px", position: "relative" }}>
-				<Autosuggest
-					suggestions={suggestions}
-					onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-					onSuggestionsClearRequested={onSuggestionsClearRequested}
-					getSuggestionValue={getSuggestionValue}
-					renderSuggestion={renderQuestion}
-					inputProps={inputProps}
-					onSuggestionSelected={onSuggestionSelected}
-					theme={theme}
-					highlightFirstSuggestion={true}
-					alwaysRenderSuggestions={true}
-				/>
-				<button
-					style={{
-						position: "absolute",
-						top: 10,
-						right: 20,
-						height: 30,
-						width: 30,
-					}}
-					onClick={() => {
-						setSearchValue("");
-						setSuggestions([]);
-						onSelect(undefined);
-					}}
-				>
-					x
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.wrapper}>
+      <form
+        onSubmit={(ev) => {
+          ev.preventDefault();
+          if (suggestions.length) {
+            const s = suggestions[selectedIndex];
+            if (s) {
+              onSelect(s.item.id);
+            }
+          }
+        }}
+      >
+        <input
+          className={styles.questionInput}
+          placeholder="Type your question..."
+          value={searchValue}
+          onChange={(ev) => setSearchValue(ev.target.value)}
+          autoFocus
+        />
+      </form>
+      {suggestions.map((s, i) => {
+        const active = i === selectedIndex;
+        return (
+          <div
+            key={s.item.id}
+            className={[styles.suggestion, active && styles.activeSuggestion]
+              .filter((v) => v)
+              .join(" ")}
+            onClick={() => onSelect(s.item.id)}
+          >
+            {s.item.text}
+            {active && <div className={styles.enterHint}>Press enter</div>}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default QuestionSelector;
