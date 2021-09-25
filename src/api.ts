3ce6a1e94d8beb938fff9db01f6779d80ced3c97
Marc Bitterli <nxt191@gmail.com>
2021-09-25 13:37:45 +0200

    more questions

--- src/api.ts
+++ src/api.ts
@@ -29,13 +29,6 @@ const sendData = async (
 };
 
 export const getQuestions = async () => await getData("/questions");
-/*export const getQuestions = async () =>
-	await [
-		{ id: 1, question: "How many people use the Line 4 each day?" },
-		{ id: 2, question: "Which Line is the busiest on weekends?" },
-		{ id: 3, question: "Which tram stop is used the fewest?" },
-		{ id: 4, question: "Which bus line is used the most?" },
-	];*/
 
 const chartSettings = [
 	{
@@ -127,6 +120,22 @@ const chartSettings = [
 for (let i = 15; i <= 57; i++) {
 	chartSettings.push({ id: i, chart_type: "map" });
 }
+for (let i = 58; i <= 59; i++) {
+	chartSettings.push({
+		id: i,
+		chart_type: "line",
+		x_axis_type: true,
+		graph_label: "asdf",
+	});
+}
+for (let i = 59; i <= 62; i++) {
+	chartSettings.push({
+		id: i,
+		chart_type: "line",
+		x_axis_type: false,
+		graph_label: "asdf",
+	});
+}
 
 export const askQuestion = async (question_id: number) => {
 	const settings = chartSettings.find((s) => s.id === question_id)!;
@@ -138,23 +147,3 @@ export const askQuestion = async (question_id: number) => {
 		graph_label: settings.graph_label,
 	} as Answer;
 };
-/*const answers = [
-	{ id: 1, data: 55.7, chart_type: "pie" },
-	{
-		id: 2,
-		data: [
-			{ x: "2019-09-07", y: 1 },
-			{ x: "2019-09-10", y: 3 },
-			{ x: "2019-09-15", y: 2 },
-			{ x: "2019-10-10", y: 4 },
-		],
-		chart_type: "line",
-		x_axis_time: true,
-	},
-	{ id: 3, data: 55.7, chart_type: "pie" },
-	{ id: 4, data: 55.7, chart_type: "pie" },
-] as Answer[];
-export const askQuestion = async (question_id: number) => {
-	await new Promise((r) => setTimeout(r, 1000));
-	return answers.find((a) => a.id === question_id);
-};*/
