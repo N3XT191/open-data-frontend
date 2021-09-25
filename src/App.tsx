f2577603eea13e468109822dd48413a1a476bcc6
Philippe Voinov <philippevoinov@gmail.com>
2021-09-25 12:23:02 +0200

    Broken triangles

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,41 @@ import AnswerPage from "./AnswerPage";
 import { getQuestions } from "./api";
 import { Question } from "./Interfaces";
 import QuestionSelector from "./QuestionSelector";
+import { Background } from "./Background";
 
 function App() {
-	const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<Question[]>([]);
 
-	const [selectedQuestion, setSelectedQuestion] = useState<
-		number | undefined
-	>();
+  const [selectedQuestion, setSelectedQuestion] = useState<
+    number | undefined
+  >();
 
-	useEffect(() => {
-		const getData = async () => {
-			const data = await getQuestions();
-			setQuestions(data);
-		};
-		getData();
-	}, []);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getQuestions();
+      setQuestions(data);
+    };
+    getData();
+  }, []);
 
-	return (
-		<>
-			{selectedQuestion ? (
-				<AnswerPage
-					question={questions.find((q) => q.id === selectedQuestion)}
-					unselectQuestion={() => setSelectedQuestion(undefined)}
-				/>
-			) : (
-				<QuestionSelector
-					questions={questions}
-					onSelect={async (id) => {
-						setSelectedQuestion(id);
-					}}
-				/>
-			)}
-		</>
-	);
+  return (
+    <>
+      {selectedQuestion ? (
+        <AnswerPage
+          question={questions.find((q) => q.id === selectedQuestion)}
+          unselectQuestion={() => setSelectedQuestion(undefined)}
+        />
+      ) : (
+        <QuestionSelector
+          questions={questions}
+          onSelect={async (id) => {
+            setSelectedQuestion(id);
+          }}
+        />
+      )}
+      <Background />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Background.tsx
@@ -0,0 +1,39 @@
+import { css } from "@emotion/css";
+
+const styles = {
+  wrapper: css`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: #eeeeee;
+    z-index: -10;
+  `,
+  triangle: css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 200%;
+    height: 200%;
+    background: red;
+  `,
+  leftTriangle: css`
+    transform: translate(-50%, 50%) rotate(55deg);
+  `,
+  rightTriangle: css`
+    transform: translate(25%, -30%) rotate(75deg);
+  `,
+};
+
+export const Background = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={[styles.triangle, styles.leftTriangle].join(" ")} />
+      <div className={[styles.triangle, styles.rightTriangle].join(" ")} />
+    </div>
+  );
+};
+
+// temp = df['2019':'2020']
+// temp = temp[temp >= 1000]
