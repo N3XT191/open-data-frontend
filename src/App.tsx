eab6c1c7f37c6896a07c082420ebe05affb6a352
Philippe Voinov <philippevoinov@gmail.com>
2021-09-25 22:50:51 +0200

    Fix charts resizing right after first render

--- src/App.tsx
+++ src/App.tsx
@@ -8,60 +8,60 @@ import { shuffle } from "lodash";
 import { Route, useHistory } from "react-router-dom";
 
 function App() {
-	const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<Question[]>([]);
 
-	const history = useHistory();
+  const history = useHistory();
 
-	useEffect(() => {
-		const getData = async () => {
-			const data = await getQuestions();
-			setQuestions(data);
-		};
-		getData();
-	}, []);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getQuestions();
+      setQuestions(data);
+    };
+    getData();
+  }, []);
 
-	useEffect(() => {
-		const onKeyDown = (ev: KeyboardEvent) => {
-			if (ev.key === "r" && (ev.altKey || ev.ctrlKey)) {
-				ev.preventDefault();
-				ev.stopPropagation();
+  useEffect(() => {
+    const onKeyDown = (ev: KeyboardEvent) => {
+      if (ev.key === "r" && (ev.altKey || ev.ctrlKey)) {
+        ev.preventDefault();
+        ev.stopPropagation();
 
-				history.push(
-					"/ask/" +
-						shuffle(
-							chartSettings.filter((s) => s.id !== 0 && s.chart_type === "line")
-						)[0]?.id
-				);
-			}
-		};
+        history.push(
+          "/ask/" +
+            shuffle(
+              chartSettings.filter((s) => s.id !== 0 && s.chart_type === "line")
+            )[0]?.id
+        );
+      }
+    };
 
-		document.addEventListener("keydown", onKeyDown);
-		return () => {
-			document.removeEventListener("keydown", onKeyDown);
-		};
-	});
+    document.addEventListener("keydown", onKeyDown);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  });
 
-	return (
-		<>
-			<div>
-				<Route
-					exact={true}
-					path="/ask/:q"
-					render={(routeProps) => (
-						<AnswerPage
-							question={questions.find(
-								(q) => q.id === +routeProps.match.params.q
-							)}
-						/>
-					)}
-				></Route>
-				<Route exact={true} path="/ask">
-					<QuestionSelector questions={questions} />
-				</Route>
-			</div>
-			<Background />
-		</>
-	);
+  return (
+    <>
+      <div>
+        <Route
+          exact={true}
+          path="/ask/:q"
+          render={(routeProps) => (
+            <AnswerPage
+              question={questions.find(
+                (q) => q.id === +routeProps.match.params.q
+              )}
+            />
+          )}
+        ></Route>
+        <Route exact={true} path="/ask">
+          <QuestionSelector questions={questions} />
+        </Route>
+      </div>
+      <Background />
+    </>
+  );
 }
 
 export default App;
--- src/Chart.tsx
+++ src/Chart.tsx
@@ -9,67 +9,38 @@ import PieChart from "./PieChart";
 import TableChart from "./TableChart";
 
 interface Props {
-	chart: Answer;
-}
-function useWindowSize() {
-	// Initialize state with undefined width/height so server and client renders match
-	// Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-	const [windowSize, setWindowSize] = useState<{
-		width: number | undefined;
-		height: number | undefined;
-	}>({
-		width: undefined,
-		height: undefined,
-	});
-	useEffect(() => {
-		// Handler to call on window resize
-		function handleResize() {
-			// Set window width/height to state
-			setWindowSize({
-				width: window.innerWidth,
-				height: window.innerHeight,
-			});
-		}
-		// Add event listener
-		window.addEventListener("resize", handleResize);
-		// Call handler right away so state gets updated with initial window size
-		handleResize();
-		// Remove event listener on cleanup
-		return () => window.removeEventListener("resize", handleResize);
-	}, []); // Empty array ensures that effect is only run on mount
-	return windowSize;
+  chart: Answer;
+  windowSize: { width: number; height: number };
 }
 
-const Chart: React.FC<Props> = ({ chart }) => {
-	const size = useWindowSize();
+const Chart: React.FC<Props> = ({ chart, windowSize }) => {
+  const maxWidth = 800;
+  const minWidth = 300;
+  const width = windowSize.width
+    ? Math.min(Math.max(windowSize.width - 400, minWidth), maxWidth)
+    : minWidth;
 
-	const maxWidth = 800;
-	const minWidth = 300;
-	const width = size.width
-		? Math.min(Math.max(size.width - 400, minWidth), maxWidth)
-		: minWidth;
+  const maxHeight = 800;
+  const minHeight = 300;
+  const height = windowSize.height
+    ? Math.min(Math.max(windowSize.height - 300, minHeight), maxHeight)
+    : minHeight;
 
-	const maxHeight = 800;
-	const minHeight = 300;
-	const height = size.height
-		? Math.min(Math.max(size.height - 300, minHeight), maxHeight)
-		: minHeight;
-
-	if (chart.chart_type === "line") {
-		return <LineChart chart={chart} width={width} height={height} />;
-	} else if (chart.chart_type === "multi-line") {
-		return <MultiLineChart chart={chart} width={width} height={height} />;
-	} else if (chart.chart_type === "pie") {
-		return <PieChart chart={chart} width={width} height={height} />;
-	} else if (chart.chart_type === "bar") {
-		return <BarChart chart={chart} width={width} height={height} />;
-	} else if (chart.chart_type === "table") {
-		return <TableChart chart={chart} width={width} height={height} />;
-	} else if (chart.chart_type === "map") {
-		return <MapChart chart={chart} width={width} height={height} />;
-	} else if (chart.chart_type === "number") {
-		return <NumberChart chart={chart} width={width} height={height} />;
-	}
-	return <>I don't know chart_type: {chart.chart_type}</>;
+  if (chart.chart_type === "line") {
+    return <LineChart chart={chart} width={width} height={height} />;
+  } else if (chart.chart_type === "multi-line") {
+    return <MultiLineChart chart={chart} width={width} height={height} />;
+  } else if (chart.chart_type === "pie") {
+    return <PieChart chart={chart} width={width} height={height} />;
+  } else if (chart.chart_type === "bar") {
+    return <BarChart chart={chart} width={width} height={height} />;
+  } else if (chart.chart_type === "table") {
+    return <TableChart chart={chart} width={width} height={height} />;
+  } else if (chart.chart_type === "map") {
+    return <MapChart chart={chart} width={width} height={height} />;
+  } else if (chart.chart_type === "number") {
+    return <NumberChart chart={chart} width={width} height={height} />;
+  }
+  return <>I don't know chart_type: {chart.chart_type}</>;
 };
 export default Chart;
