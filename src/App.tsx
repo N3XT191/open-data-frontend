ac9bd042e601dd9df6f21054ac14db5121e0fa2f
Philippe Voinov <philippevoinov@gmail.com>
2021-09-26 10:14:30 +0200

    Redirect from homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
+import { AnimateSharedLayout, motion } from "framer-motion";
+import { groupBy, shuffle } from "lodash";
 import { useEffect, useState } from "react";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import AnswerPage from "./AnswerPage";
+import AnswerPagePoster from "./AnswerPagePoster";
 import { chartSettings, getQuestions } from "./api";
+import Background from "./Background";
 import { Question } from "./Interfaces";
 import QuestionSelector from "./QuestionSelector";
-import Background from "./Background";
-import { shuffle, groupBy } from "lodash";
-import { Route, useHistory } from "react-router-dom";
-import AnswerPagePoster from "./AnswerPagePoster";
 import { useWindowSize } from "./use-window-size";
-import {
-  AnimatePresence,
-  AnimateSharedLayout,
-  motion,
-  Variants,
-} from "framer-motion";
 
 function App() {
   const [questions, setQuestions] = useState<Question[]>([]);
@@ -72,52 +67,58 @@ function App() {
     <>
       <AnimateSharedLayout type="crossfade">
         <div style={{ height: "100%" }}>
-          <Route
-            exact={true}
-            path="/ask/:q"
-            render={(routeProps) => {
-              const question = questions.find(
-                (q) => q.id === +routeProps.match.params.q
-              );
-              return (
-                question && (
-                  <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                  >
-                    <AnswerPage question={question} windowSize={windowSize} />
-                  </motion.div>
-                )
-              );
-            }}
-          ></Route>
-          <Route
-            exact={true}
-            path="/poster/:q"
-            render={(routeProps) => {
-              const question = questions.find(
-                (q) => q.id === +routeProps.match.params.q
-              );
-              return (
-                question && (
-                  <AnswerPagePoster
-                    question={question}
-                    windowSize={windowSize}
-                  />
-                )
-              );
-            }}
-          ></Route>
-          <Route exact={true} path="/ask">
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, translateY: 0 }}
-              exit={{ opacity: 0, translateY: 200 }}
-            >
-              <QuestionSelector questions={questions} windowSize={windowSize} />
-            </motion.div>
-          </Route>
+          <Switch>
+            <Route
+              exact={true}
+              path="/ask/:q"
+              render={(routeProps) => {
+                const question = questions.find(
+                  (q) => q.id === +routeProps.match.params.q
+                );
+                return (
+                  question && (
+                    <motion.div
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                    >
+                      <AnswerPage question={question} windowSize={windowSize} />
+                    </motion.div>
+                  )
+                );
+              }}
+            ></Route>
+            <Route
+              exact={true}
+              path="/poster/:q"
+              render={(routeProps) => {
+                const question = questions.find(
+                  (q) => q.id === +routeProps.match.params.q
+                );
+                return (
+                  question && (
+                    <AnswerPagePoster
+                      question={question}
+                      windowSize={windowSize}
+                    />
+                  )
+                );
+              }}
+            ></Route>
+            <Route exact={true} path="/ask">
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                exit={{ opacity: 0, translateY: 200 }}
+              >
+                <QuestionSelector
+                  questions={questions}
+                  windowSize={windowSize}
+                />
+              </motion.div>
+            </Route>
+            <Redirect to="/ask" />
+          </Switch>
         </div>
       </AnimateSharedLayout>
       <Background windowSize={windowSize} />
