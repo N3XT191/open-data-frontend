bbf4e018af4c2b0106cf98ed3b93b5bc7f874b51
Philippe Voinov <philippevoinov@gmail.com>
2021-09-26 09:54:32 +0200

    Support setting API URL

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import { Answer } from "./Interfaces";
 
-const baseURL = "http://127.0.0.1:8000";
+const baseURL = process.env.API_URL || "http://127.0.0.1:8000";
 
 const getData = async (route: string) => {
   const res = await fetch(baseURL + route);
